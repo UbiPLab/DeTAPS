92013b93cf95ec65b985f9c8c23840144ed43bcc
UbiPLab <69721628+UbiPLab@users.noreply.github.com>
2023-09-25 20:59:59 +0800

    Update Commit.py

--- main/tools/Commit.py
+++ main/tools/Commit.py
@@ -1,13 +1,21 @@
 import hashlib
 
+q = 264926624393126082647786234614522411169
+h = 34970314419892642909507782969116958274309
+g = 27464808337291794528868661597410061338052
 
 def commit(pk, r):
-    hkp1 = hashlib.sha256((" ".join(map(str, pk)) + str(r)).encode()).hexdigest()
-    c = int(hkp1, 16)
+    hkp1 = hashlib.sha256((" ".join(map(str, pk))).encode()).hexdigest()
+    m = int(hkp1, 16)
+    c0 = pow(g,r,q)
+    c1 = m * pow(h,r,q)
+    c = (c0, c1)
     return c
 
 
 def verify(pk, r, c):
-    hkp1 = hashlib.sha256((" ".join(map(str, pk)) + str(r)).encode()).hexdigest()
-    com = int(hkp1, 16)
-    return com == c
+    hkp1 = hashlib.sha256((" ".join(map(str, pk))).encode()).hexdigest()
+    m = int(hkp1, 16)
+    c0 = pow(g,r,q)
+    c1 = m * pow(h,r,q)
+    return c0 == c[0] and c1 == c[1]
